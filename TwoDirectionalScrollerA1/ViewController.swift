d74115a2c439e579791a8358cae833774ba69fac
Jakkawad <admin@all2sale-imac.local>
2016-07-13 11:03:56 +0700

    Initial Commit

---
+++ TwoDirectionalScrollerA1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwoDirectionalScrollerA1
+//
+//  Created by admin on 7/13/2559 BE.
+//  Copyright © 2559 All2Sale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
